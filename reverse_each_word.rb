c0012d75371d058ad40da1e7a1d505b8a7b87c86
Alan sanchez <asanchezhaza@gmail.com>
2020-10-26 10:57:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,9 @@
 def reverse_each_word(sentence)
   new_array = sentence.split (" ")
-  reversed_array = 
-  sentence.each { |string|
-    string.reverse
-     string
-  }
+  reversed_array = new_array.each {|x| x.reverse!}
+  
+ 
+  
   
   
   

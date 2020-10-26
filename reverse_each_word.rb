5f3e19f13cb87679f35c91e817bfb0427133bdf3
Alan sanchez <asanchezhaza@gmail.com>
2020-10-26 10:58:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   new_array = sentence.split (" ")
-  reversed_array = new_array.each {|x| x.reverse!}
+  reversed_array = new_array.collect {|x| x.reverse!}
   reversed_array.join(" ")
  
   
